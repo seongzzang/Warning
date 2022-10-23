f5f286a7e6b33bf0642d358354d917ab5a739630
seongzzang <87599027+seongzzang@users.noreply.github.com>
2022-10-23 22:04:38 +0900

    Initial Commit

---
+++ Warning/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Warning
+//
+//  Created by 양성혜 on 2022/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
